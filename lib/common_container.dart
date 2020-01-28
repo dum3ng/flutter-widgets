3546cd48441cfa23574bfa30d528829aa2626434
dum3ng <dumeng81@outlook.com>
2020-01-28 12:06:10 +0800

    init

---
+++ lib/common_container.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CommonContainer extends StatelessWidget {
+  final child;
+  final title;
+
+  CommonContainer({this.child, this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: child,
+      appBar:  title !=null ? AppBar(title: Text(title),) : null,
+      floatingActionButton: FloatingActionButton(
+        child: Text('back'),
+        onPressed: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
